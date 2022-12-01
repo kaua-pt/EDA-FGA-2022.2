66130ab5fc8bbdcbeb7883f77a660b854e5bd675
Kauã Vinícius <81399319+kaua-pt@users.noreply.github.com>
2022-12-01 13:23:26 -0300

    Notebook changes

---
+++ main.c
@@ -0,0 +1,51 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    char timesQ[8],timesS[4],timesF[2],timeV;
+    int t1,t2,i;
+    
+    for(i=0; i<8;i++){
+        scanf("%d %d",&t1,&t2);
+        if(t1>t2){
+            timesQ[i] = (char) 65 + (2*i);
+        }else{
+            timesQ[i] = (char) 65 + (2*i) + 1;
+        }
+    }
+    
+    for(i=0; i<4;i++){
+        scanf("%d %d",&t1,&t2);
+        if(t1>t2){
+            timesS[i] = timesQ[2*i];
+        }else{
+            timesS[i] = timesQ[2*i+1];
+        }
+    }
+    
+    for(i=0; i<2;i++){
+        scanf("%d %d",&t1,&t2);
+        if(t1>t2){
+            timesF[i] = timesS[2*i];
+        }else{
+            timesF[i] = timesS[2*i+1];
+        }
+    }
+    
+    scanf("%d %d",&t1,&t2);
+    if(t1>t2){
+        timeV = timesF[0];
+    }else{
+        timeV = timesF[1];
+    }
+    printf("%c", timeV);
+    return 0;
+}
